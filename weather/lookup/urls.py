0caf8a8a8d2063248342a6fa503595217226ec58
YC Jhuo <y.c.jhuo@gmail.com>
2020-03-08 12:50:31 -0400

    Done this weather app, but need to fix the error page

--- weather/lookup/urls.py
+++ weather/lookup/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+	#http://www.airnowapi.org/aq/observation/zipCode/current/?format=application/json&zipCode=07310&distance=5&API_KEY=5EC6FBC6-7EFD-40B2-A94D-57D35B4A71D6
     path('', views.home, name="home"),
     path('about.html', views.about, name="about"),
 ]
